93c237cb7d1545bf7c1497058c25e92d8e5b020b
hncheung23 <marco960525@gmail.com>
2021-12-26 23:09:51 +0800

    remove file

--- src/App.js
+++ src/App.js
@@ -5,23 +5,55 @@ import Books from "./components/books/books";
 import BookDetail from "./components/book-detail/book-detail";
 import Breadcrumbs from "./components/breadcrumbs/breadcrumbs";
 import SearchBar from "./components/search-bar/search-bar";
+import { makeStyles } from "@material-ui/core/styles";
 
 function App() {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      position: 'relative',
+      height: '70vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    title: {
+      textAlign: 'center',
+    },
+    container: {
+      margin: '10vh',
+    }
+  }));
+
+  const classes = useStyles();
+
   const welcomeText = "Try to start with typing somethings";
   const navigate = useNavigate();
+  const renderWelcome = () => {
+    return (
+      <div className={classes.root}>
+        <div><h1 className={classes.title}>{welcomeText}</h1></div>
+      </div>
+    );
+  };
   return (
-    <div>
+    <div className={classes.container}>
       <SearchBar navigate={navigate} />
       <Breadcrumbs navigate={navigate} />
       <Routes>
-        <Route path="/" element={<div>{welcomeText}</div>} />
-        <Route path="/authors/:searchingWords" element={<Authors navigate={navigate}/>} />
-        <Route path="/authors/:searchingWords/:authorsId/books" element={<Books navigate={navigate}/>} />
+        <Route path="/" element={renderWelcome()} />
+        <Route
+          path="/authors/:searchingWords"
+          element={<Authors navigate={navigate} />}
+        />
+        <Route
+          path="/authors/:searchingWords/:authorsId/books"
+          element={<Books navigate={navigate} />}
+        />
         <Route
           path="/authors/:searchingWords/:authorsId/books/:bookId"
           element={<BookDetail />}
         />
-        <Route path="*" element={<div>{welcomeText}</div>} />
+        <Route path="*" element={renderWelcome()} />
       </Routes>
     </div>
   );
